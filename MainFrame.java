03fae6f7153f6ede1445d0273870bf9e678cafaf
CrPasc-Github <44143240+CrPasc-Github@users.noreply.github.com>
2020-11-15 09:37:13 -0700

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -1,3 +1,18 @@
+/**
+ * CSE360 Final Project
+ * This program loads a roster, adds attendance, save and plots data.
+ * 
+ * @author
+ * @author
+ * @author
+ * @author
+ * @author
+ *
+ * @since 2020-11-12
+ * 
+ * Class ID: 70605  
+ */
+
 import java.util.Scanner;
 import java.awt.*;
 import javax.swing.*;
@@ -10,46 +25,61 @@ public class MainFrame extends JFrame //implements ActionListener
     static JMenuBar mymenubar; 
   
     // JMenu 
-    static JMenu componentX,componentY; 
+    static JMenu componentX, componentY; 
   
     // Menu items 
-    static JMenuItem menuitem1, menuitem2, menuitem3,menuitem4;
+    static JMenuItem menuitem1, menuitem2, menuitem3, menuitem4;
 
-
+    //constructor
     MainFrame(){
         
     }
 
+    /**
+    *
+    * main method that has the JFrame gui
+    * @param args unsused
+    *
+    **/
     public static void main(String[] args ){
 
+        //create a MainFrame object
         MainFrame m = new MainFrame();
+        
+        //window title
         m.setTitle("CSE360 Final Project");
+        
         m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   
-
+        //create a Jmenubar
         mymenubar = new JMenuBar(); 
+        
+        //create two new Jmenu objects for "File" and "About"
         componentX = new JMenu("File"); 
         componentY = new JMenu("About");
-
+        
+        //create menu items for "File"
         menuitem1 = new JMenuItem("Load a Roaster"); 
         menuitem2 = new JMenuItem("Add attendance"); 
         menuitem3 = new JMenuItem("Save");
         menuitem4 = new JMenuItem("Plot Data");
 
-       
-
+        //add menu items to "File"
         componentX.add(menuitem1); 
         componentX.add(menuitem2); 
         componentX.add(menuitem3);
         componentX.add(menuitem4);
 
-
+        //add the "File" and "About" to the menubar
         mymenubar.add(componentX); 
         mymenubar.add(componentY);
 
         m.setJMenuBar(mymenubar); 
-
+        
+        //set the frame size to 500x500
         m.setSize(500,500);
+        
+        //make the frame visible
         m.setVisible(true); 
 
         /*
