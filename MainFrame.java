cd1de5fdf0e7ed9f434666e93de53aed01d59325
manaliRedkar <msredkar28@gmail.com>
2020-11-21 13:11:40 -0800

    About working

--- MainFrame.java
+++ MainFrame.java
@@ -13,27 +13,84 @@
  * Class ID: 70605  
  */
 
-import java.util.Scanner;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 
-public class MainFrame extends JFrame //implements ActionListener 
+public class MainFrame extends JFrame implements MenuListener
 {
 
-    static JMenuBar mymenubar; 
+      JMenuBar mymenubar; 
   
     // JMenu 
-    static JMenu componentX, componentY; 
+      JMenu file, about; 
   
     // Menu items 
-    static JMenuItem menuitem1, menuitem2, menuitem3, menuitem4;
+      JMenuItem menuitem1, menuitem2, menuitem3, menuitem4, ab;
 
     //constructor
-    MainFrame(){
+    public MainFrame()
+    {
+    	
+    	setLayout(new FlowLayout());
+    	 //create a Jmenubar
+        mymenubar = new JMenuBar(); 
+        add(mymenubar);
+
+        //create two new Jmenu objects for "File" and "About"
+        file = new JMenu("File"); 
+        about = new JMenu("About");
         
+        menuitem1 = new JMenuItem("Load a Roaster"); 
+        menuitem2 = new JMenuItem("Add attendance"); 
+        menuitem3 = new JMenuItem("Save");
+        menuitem4 = new JMenuItem("Plot Data");
+        
+        file.add(menuitem1); 
+        file.add(menuitem2); 
+        file.add(menuitem3);
+        file.add(menuitem4);
+        
+        mymenubar.add(file);
+        mymenubar.add(about);
+
+        setJMenuBar(mymenubar);
+
+        about.addMenuListener(this);
+
     }
+    
+    @Override
+    public void menuSelected(MenuEvent e)
+    {
+    	 
+    	 AboutWindow gui = new AboutWindow(MainFrame.this);
+    	 gui.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+    	 gui.setSize(300,300);
+    	 gui.setLocation(300,300);
+    	 gui.setVisible(true);
+    
+    }
+    @Override
+    public void menuDeselected(MenuEvent e)
+    {
+    	 
+    
+    }
+    
+    @Override
+    public void menuCanceled(MenuEvent e)
+    {
+    	 
+    
+    }
+    
 
     /**
     *
@@ -41,80 +98,26 @@ public class MainFrame extends JFrame //implements ActionListener
     * @param args unsused
     *
     **/
+    
+
     public static void main(String[] args ){
 
         //create a MainFrame object
         MainFrame m = new MainFrame();
         
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         //window title
         m.setTitle("CSE360 Final Project");
-        
-        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  
-        //create a Jmenubar
-        mymenubar = new JMenuBar(); 
-        
-        //create two new Jmenu objects for "File" and "About"
-        componentX = new JMenu("File"); 
-        componentY = new JMenu("About");
-        
-        //create menu items for "File"
-        menuitem1 = new JMenuItem("Load a Roaster"); 
-        menuitem2 = new JMenuItem("Add attendance"); 
-        menuitem3 = new JMenuItem("Save");
-        menuitem4 = new JMenuItem("Plot Data");
 
-        //add menu items to "File"
-        componentX.add(menuitem1); 
-        componentX.add(menuitem2); 
-        componentX.add(menuitem3);
-        componentX.add(menuitem4);
-
-        //add the "File" and "About" to the menubar
-        mymenubar.add(componentX); 
-        mymenubar.add(componentY);
-
-        m.setJMenuBar(mymenubar); 
-        
         //set the frame size to 500x500
         m.setSize(500,500);
         
         //make the frame visible
         m.setVisible(true); 
 
-        /*
-        JTabbedPane t = new JTabbedPane();
-        panel1 = new JPanel();
-        panel2 = new JPanel();
-
-        t.addTab("File", panel1);
-        t.addTab("About", panel2);
-        m.add(t);
-        */
-        
-    }   
-
     
-    /*
-    public void actionPerformed(ActionEvent e) 
-    {
-        String s = e.getActionCommand(); 
-        if (s.equals("About")) { 
-            // create a dialog Box 
-            JDialog d = new JDialog(y, "dialog Box"); 
-  
-            // create a label 
-            JLabel l = new JLabel("this is a dialog box"); 
-  
-            d.add(l); 
-  
-            // setsize of dialog 
-            d.setSize(100, 100); 
-  
-            // set visibility of dialog 
-            d.setVisible(true); 
-        } 
-        
-    } 
-    */
+    
+   }
+
 }
