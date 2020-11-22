0daab1720ceab30bde6cd1ef157870997585a801
manaliRedkar <msredkar28@gmail.com>
2020-11-21 16:35:24 -0800

    Load a Roaster Trial 1

---
+++ LoadRoaster.java
@@ -0,0 +1,124 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.AbstractTableModel;
+
+public class LoadRoaster extends JPanel {
+    private final JTable table;
+
+    public LoadRoaster(JFrame frame1) {
+        //super(frame1,"Load a Roaster", true);
+        setLayout(new BorderLayout ());
+        //super(frame1, "Load Roaster", true);
+        this.table = new JTable(new MyModel());
+        this.table.setPreferredScrollableViewportSize(new Dimension(700, 70));
+        this.table.setFillsViewportHeight(true);
+        this.table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        JPanel ButtonOpen = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        add(ButtonOpen, BorderLayout.SOUTH);
+        // Create the scroll pane and add the table to it.
+        JScrollPane scrollPane = new JScrollPane(table);
+        // Add the scroll pane to this panel.
+        add(scrollPane, BorderLayout.CENTER);
+        // add a nice border
+        setBorder(new EmptyBorder(5, 5, 5, 5));
+        CSVFile Rd = new CSVFile();
+        MyModel NewModel = new MyModel();
+        this.table.setModel(NewModel);
+        File DataFile = new File("Student.csv");
+        ArrayList<String[]> Rs2 = Rd.ReadCSVfile(DataFile);
+        NewModel.AddCSVData(Rs2);
+        System.out.println("Rows: " + NewModel.getRowCount());
+        System.out.println("Cols: " + NewModel.getColumnCount());
+    }
+
+    // Method for reading CSV file
+    public class CSVFile {
+        private final ArrayList<String[]> Rs = new ArrayList<String[]>();
+        private String[] OneRow;
+
+        public ArrayList<String[]> ReadCSVfile(File DataFile) {
+            try {
+                BufferedReader brd = new BufferedReader(new FileReader(DataFile));
+                while (brd.ready()) {
+                    String st = brd.readLine();
+                    OneRow = st.split(",|\\s|;");
+                    Rs.add(OneRow);
+                    System.out.println(Arrays.toString(OneRow));
+                } // end of while
+            } // end of try
+            catch (Exception e) {
+                String errmsg = e.getMessage();
+                System.out.println("File not found:" + errmsg);
+            } // end of Catch
+            return Rs;
+        }// end of ReadFile method
+    }// end of CSVFile class
+
+    private static void createAndShowGUI() {
+        // Create and set up the window.
+        JFrame frame = new JFrame("LoadRoaster");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Create and set up the content pane.
+        LoadRoaster newContentPane = new LoadRoaster();
+        frame.setContentPane(newContentPane);
+        // Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    class MyModel extends AbstractTableModel {
+        //Your CSV needs to be same number of columns as this array here
+        private final String[] columnNames = { "ID", "FS", "LS", "PP", "ACAD", "ASUrite","1","2","3","4","5","6","7"};
+        private ArrayList<String[]> Data = new ArrayList<String[]>();
+
+        public void AddCSVData(ArrayList<String[]> DataIn) {
+            this.Data = DataIn;
+            this.fireTableDataChanged();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return columnNames.length;// length;
+        }
+
+        @Override
+        public int getRowCount() {
+            return Data.size();
+        }
+
+        @Override
+        public String getColumnName(int col) {
+            return columnNames[col];
+        }
+
+        @Override
+        public Object getValueAt(int row, int col) {
+            return Data.get(row)[col];
+        }
+    }
+
+    
+    
+    public static void main(String[] args) {
+        // Schedule a job for the event-dispatching thread:
+        // creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+}
